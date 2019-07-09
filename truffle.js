408619182decbe89f2fbf7b8b1c7be65f4cfa583
eunji7122 <51905894+eunji7122@users.noreply.github.com>
2019-07-09 21:50:23 +0900

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ const PrivateKeyConnector = require('connect-privkey-to-provider')  // 라이브
 const NETWORK_ID = '1001' // 바오밥 고유의 아이디
 const GASLIMIT = '20000000'  // 배포하는데 들어가는 가스 한도
 const URL = 'https://api.baobab.klaytn.net:8651'
-const PRIVATE_KEY = '0xc391960963382589542407169cd7e8238b4bd07f459b23bfc6e418dee1b296fd'  // 계정01의 비밀키
+const PRIVATE_KEY = '비밀키 입력'  // 계정01의 비밀키
 
 // 위 설정들을 module.exports에 사용
 module.exports = {
